ddc93dd968c39fd48cd8f7b788c7ef347d8ff104
salman <1025618984@qq.com>
2021-09-10 18:03:11 +0800

    下班

--- src/two/Code02RandToRand.java
+++ src/two/Code02RandToRand.java
@@ -6,14 +6,25 @@ import java.util.Arrays;
 public class Code02RandToRand {
   public static void main(String[] args) {
     int testTime = 100000;
-    int count=10;
-    int[] arr=new int[count];
+    int count = 10;
+    int[] arr = new int[count];
     for (int i = 0; i < testTime; i++) {
-      //int num = equalProbably1To5();
-      int num = equalProbably0To1();
+      // int num = equalProbably1To5();
+      // int num = equalProbably0To1();
+      int num = equalProbably1To6();
       arr[num]++;
     }
     System.out.println(Arrays.toString(arr));
+  }
+
+  public static int equalProbably1To6() {
+    int ans = 7;
+    do {
+      // int num = equalProbably0To1();
+      //      ans = num + (num << 1) + (num << 2);
+      ans = equalProbably0To1() + (equalProbably0To1() << 1) + (equalProbably0To1() << 2);
+    } while (ans == 7);
+    return ans;
   }
 
   /**
@@ -30,6 +41,6 @@ public class Code02RandToRand {
     do {
       ans = equalProbably1To5();
     } while (ans == 3);
-    return ans>3?0:1;
+    return ans > 3 ? 0 : 1;
   }
 }
